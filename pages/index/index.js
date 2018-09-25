bdbbf1518cc739a9679b79cf2e8a006bb8f0317f
lihaibo <340063567@qq.com>
2018-09-25 11:16:14 +0800

    3 commit

---
+++ pages/index/index.js
@@ -0,0 +1,89 @@
+//js
+Page({
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+            }
+          })
+        }
+      } 
+    })
+  },
+  bindGetUserInfo: function (event) {
+    var _this = this;
+    //使用
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.login({
+            success: function (res) {
+              var code = res.code;//登录凭证
+              if (code) {
+                //2、调用获取用户信息接口
+                wx.getUserInfo({
+                  success: function (res) {
+                    //console.log({ encryptedData: res.encryptedData, iv: res.iv, code: code })
+                    //3.请求自己的服务器，解密用户信息 获取unionId等加密信息
+                    wx.request({
+                      url: 'http://localhost:8080/kin/gh/login/login',//自己的服务接口地址
+                      method: 'post',
+                      data: { encryptedData: res.encryptedData, iv: res.iv, code: code },
+                      success: function (data) {
+                        var userInfo_ = data.data.userInfo;
+                        var app = getApp();
+                        app.globalData.userInfo = userInfo_;
+                        // //4.解密成功后 获取自己服务器返回的结果
+                        if (data.data.result == 1) {
+                          wx.redirectTo({
+                            url: '/pages/patient/patient',
+                          })
+                        } else if (data.data.result == 2){
+                          _this.setData(
+                            { popErrorMsg: "请联系后台人员维护您的基本信息！" }
+                          );
+                          _this.ohShitfadeOut();
+                        } else if (data.data.result == 3) {
+                          _this.setData(
+                            { popErrorMsg: "权限已被禁用！" }
+                          );
+                          _this.ohShitfadeOut();
+                        }else {
+                          _this.setData(
+                            { popErrorMsg: "授权登录失败！" }
+                          );
+                          _this.ohShitfadeOut();
+                        }
+                      }
+                    })
+                  }
+                })
+              } else {
+                _this.setData(
+                  { popErrorMsg: "登录失败！" }
+                );
+                _this.ohShitfadeOut();
+              }
+            }
+          })
+        }
+
+      }
+    })
+  },
+  ohShitfadeOut() {
+    var fadeOutTimeout = setTimeout(() => {
+      this.setData({ popErrorMsg: '' });
+      clearTimeout(fadeOutTimeout);
+    }, 3000);
+  }
+})
---
+++ pages/patient/patient.js
@@ -0,0 +1,156 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    modalHidden: true,
+    patientSex: '',
+    patientAge: '',
+    patientName: '',
+    patientPhoneNumber: '',
+    patientCardNo:'',
+    patientAddress:''
+  },
+  modalTap: function (e) {
+    this.setData({
+      modalHidden: false
+    })
+  },
+  formSubmit: function(e) {
+    var _this = this;
+    this.setData({
+      patientSex: e.detail.value.patientSex
+    })
+    this.setData({
+      patientAge: e.detail.value.patientAge
+    })
+    this.setData({
+      patientCardNo: e.detail.value.patientCardNo
+    })
+    this.setData({
+      patientName: e.detail.value.patientName
+    })
+    this.setData({
+      patientPhoneNumber: e.detail.value.patientPhoneNumber
+    })
+    this.setData({
+      patientAddress: e.detail.value.patientAddress
+    })
+    if (this.data.patientName.length == 0) {
+      console.log("aaa");
+      this.setData(
+        { popErrorMsg: "姓名不能为空!" }
+      );
+      this.ohShitfadeOut();
+      return;
+      // wx.showToast({
+      //   title: '姓名不能为空!',
+      //   icon: 'loading',
+      //   duration: 1000
+      // })
+      // return;
+    } else if (this.data.patientAge.length == 0) {
+      this.setData(
+        { popErrorMsg: "年龄信息输入有误!" }
+      );
+      this.ohShitfadeOut();
+      return;
+      // wx.showToast({
+      //   title: '年龄信息输入有误!',
+      //   icon: 'loading',
+      //   duration: 1000
+      // })
+      // return;
+    } else if (this.data.patientCardNo.length == 0 || this.data.patientCardNo.length < 18) {
+      this.setData(
+        { popErrorMsg: "身份证信息输入有误!" }
+      );
+      this.ohShitfadeOut();
+      return;
+      // wx.showToast({
+      //   title: '身份证信息有误!',
+      //   icon: 'loading',
+      //   duration: 1000
+      // })
+      // return;
+    } else if (this.data.patientPhoneNumber.length == 0 || this.data.patientPhoneNumber.length < 11) {
+      this.setData(
+        { popErrorMsg: "手机号输入有误!" }
+      );
+      this.ohShitfadeOut();
+      return;
+      // wx.showToast({
+      //   title: '手机号有误!',
+      //   icon: 'loading',
+      //   duration: 1000
+      // })
+      // return;
+    }
+    this.modalTap();
+  },
+  confirm_one: function (e) {
+    var _this = this;
+    console.log(e);
+    this.setData({
+      modalHidden: true,
+      toast1Hidden: false,
+      notice_str: '提交成功'
+    })
+    wx.request({
+      url: 'http://localhost:8080/kin/gh/patient/save',
+      data: {
+        patientSex: this.data.patientSex,
+        patientAge: this.data.patientAge,
+        patientName: this.data.patientName,
+        patientPhoneNumber: this.data.patientPhoneNumber,
+        patientCardNo: this.data.patientCardNo,
+        patientAddress: this.data.patientAddress,
+        openId: getApp().globalData.userInfo.openId
+      },
+      method: 'POST',
+      success: function (res) {
+        if (res.data.result == 1) {
+          wx.showToast({
+            title: '录入成功！',
+            icon: 'loading',
+            duration: 1500
+          })
+          _this.setData({
+            patientSex: '1',
+            patientAge: '',
+            patientName: '',
+            patientPhoneNumber: '',
+            patientCardNo: '',
+            patientAddress: ''
+          })
+        } else {
+          wx.showToast({
+            title: '录入失败',
+            icon: 'loading',
+            duration: 1500
+          })
+        }
+
+      },
+      fail: function () {
+        console.log('系统错误')
+      }
+    })
+    },
+  cancel_one: function (e) {
+    console.log(e);
+    this.setData({
+      modalHidden: true,
+      toast1Hidden: false,
+      notice_str: '取消成功'
+    });
+  },
+  ohShitfadeOut() {
+    var fadeOutTimeout = setTimeout(() => {
+      this.setData({ popErrorMsg: '' });
+      clearTimeout(fadeOutTimeout);
+    }, 3000);
+    }
+
+})
